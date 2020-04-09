f978f4e8ebe398668d25a29f0d01812da93c7389
iatraljic <iatraljic@gmail.com>
2020-04-09 21:00:15 +0200

    Added sort

--- routes/tracks.js
+++ routes/tracks.js
@@ -9,17 +9,24 @@ class TracksDAO {
     this.db = db;
   }
 
+  // --------------------------------------------------------------------------
+  // ----- getAll
+  // -----
+  // --------------------------------------------------------------------------
   getAll(callback) {
-    response.data = this.db;
+    response.data = this.db.tracks.data;
     callback(response);
   }
 
+  // --------------------------------------------------------------------------
+  // ----- getByID
+  // -----
+  // --------------------------------------------------------------------------
   getByID(id, callback) {
     let resData = {};
 
     for (let i = 0; i < this.db.tracks.data.length; i++) {
       const el = this.db.tracks.data[i];
-      console.log(el.id, id);
 
       if (String(el.id) === id) {
         resData = el;
@@ -31,8 +38,39 @@ class TracksDAO {
     callback(response);
   }
 
+  // --------------------------------------------------------------------------
+  // ----- getSorted
+  // -----
+  // --------------------------------------------------------------------------
   getSorted(sortBy, callback) {
-    let resData = { sortBy };
+    const resData = this.db.tracks.data;
+
+    if (sortBy === 'name') {
+      resData.sort((a, b) => {
+        // Case insensitive
+        const titleA = a.title.toUpperCase();
+        const titleB = b.title.toUpperCase();
+
+        if (titleA < titleB) {
+          return -1;
+        } else if (titleA > titleB) {
+          return 1;
+        }
+        return 0;
+      });
+    } else if (sortBy === 'duration') {
+      resData.sort((a, b) => {
+        const durationA = a.duration;
+        const durationB = b.duration;
+
+        if (durationA < durationB) {
+          return -1;
+        } else if (durationA > durationB) {
+          return 1;
+        }
+        return 0;
+      });
+    }
 
     response.data = resData;
     callback(response);
